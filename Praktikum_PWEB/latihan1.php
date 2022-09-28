694902de804ba73590eb2fd6653999b133fa270a
Aditia W <112836779+A-W-E-E@users.noreply.github.com>
2022-09-28 17:49:57 +0700

    Add files via upload

---
+++ Praktikum_PWEB/latihan1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Praktikum 1</title>
+    </head>
+    <body bgcolor="#ADD8E6">
+        <h1 align="center" style="background-color:aqua"> HELLO WORD</h1>
+        <img src="logo.jpg" width="25%" align="left" hspace="10"/>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <hr width="90%">
+    <h2 style="border:2px solid aqua"> HELLO WORD </h2> 
+    
+    <table border="2px" align="center" wisth="80%">
+        <thead bgcolor="green">
+        <h2 align="center">Data Mahasiswa</h2>
+
+            <th>NO</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Kelas</th>
+            
+        </thead>
+        <tbody bgcolor="yellow">
+        <tr>
+                <td>1</td>
+                <td>Aditia W</td>
+                <td>20SA1186</td>
+                <td rowspan="3">IF20A</td>
+            </tr>
+            
+            <tr>
+                <td>2</td>
+                <td>Aditia W</td>
+                <td>20SA1186</td>
+                
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Aditia W</td>
+                <td>20SA1186</td>
+                
+            </tr>
+            <tr>
+            <tr >
+                <td colspan="3"> FOOTER</td>
+                <td>oke</td>
+            </tr>
+            </tr>
+        </tbody>
+    </table>
+    
+    <ul type="square">
+        <li>Pendahuluan</li>
+        <ol type="A">
+            <li>Latar Belakang</li>
+            <li>Rumusan Masalah</li>
+            <li>Tujuan</li>
+        </ol>
+    </ul>
+
+    <h5>Logo Ig</h5>
+    <a href="https://www.instagram.com/?hl=id"> Klik untuk login ig</a> <br>
+    atau Klik gambar dibawah
+    <a href="https://www.instagram.com/?hl=id"> <br> 
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png" alt="" width="5%">
+    </a>
+    </body>
+</html>
